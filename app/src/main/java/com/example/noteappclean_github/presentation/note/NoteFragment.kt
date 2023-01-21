24b7cfc0ac1b782e1c7f58670e99b3bfe4a5a751
Hossein <hossein.arabameri69@gmail.com>
2023-01-21 19:57:57 +0330

    Get Data from Spinner in NoteFragment

--- app/src/main/java/com/example/noteappclean_github/presentation/note/NoteFragment.kt
+++ app/src/main/java/com/example/noteappclean_github/presentation/note/NoteFragment.kt
@@ -5,14 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.viewModels
+import androidx.fragment.app.viewModels
 import com.example.noteappclean_github.R
 import com.example.noteappclean_github.databinding.FragmentNoteBinding
+import com.example.noteappclean_github.util.setupListWithAdapter
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class NoteFragment : BottomSheetDialogFragment() {
         private lateinit var binding: FragmentNoteBinding
+    private val viewModel: NoteViewModel by viewModels()
+    private var category = ""
+    private var priority = ""
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentNoteBinding.inflate(layoutInflater)
@@ -21,7 +28,22 @@ class NoteFragment : BottomSheetDialogFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        binding.apply {
+            //Close
+            closeImg.setOnClickListener { dismiss() }
+            //Spinner Category
+            viewModel.loadCategoriesData()
+            viewModel.categoriesList.observe(viewLifecycleOwner) {
+                categoriesSpinner.setupListWithAdapter(it) { itItem ->
+                    category = itItem
+                }
+            }
+            //Spinner priority
+            viewModel.loadPrioritiesData()
+            viewModel.prioritiesList.observe(viewLifecycleOwner) {
+                prioritySpinner.setupListWithAdapter(it) { itItem -> priority = itItem }
+            }
+        }
     }
 
 }
